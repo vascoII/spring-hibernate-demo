0ff1d1a5006e75758c31776d5bebfbf22af60cf7
Fabrice Da Silva <f-dasilva2@autodistribution.com>
2020-05-10 18:36:17 +0200

    Added: mysql docker container and jdbc connector

---
+++ src/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezome=UTC";
+		String user = "dev";
+		String pwd = "dev";
+		
+		try {
+			System.out.println("Commecting to db: " + jdbcUrl);
+			Connection myCon = 
+					DriverManager.getConnection(jdbcUrl, user, pwd);
+			System.out.println("Commection successfull");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
