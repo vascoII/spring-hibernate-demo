66db19f8c15cac034ca2be0fd53eba00a9651a80
Fabrice Da Silva <f-dasilva2@autodistribution.com>
2020-05-11 17:08:44 +0200

    Added: query entity

---
+++ src/springdemo/QueryStudent.java
@@ -0,0 +1,71 @@
+package springdemo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import springdemo.entity.Student;
+
+public class QueryStudent {
+
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start Transaction
+			System.out.println("BeginTransaction");
+			session.beginTransaction();
+			//List
+			List<Student> allStudents = session
+					.createQuery("from Student")
+					.getResultList();
+			System.out.println("allStudents: ");
+			for (Student tempStudent : allStudents) {
+				System.out.println(tempStudent);
+			}
+			//commit transaction
+			session.getTransaction().commit();
+			
+			//get new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			//List with where
+			List<Student> whereStudents = session
+					.createQuery("from Student s where s.lastName LIKE '%o%'")
+					.getResultList();
+			System.out.println("whereStudents: ");
+			for (Student tempStudent : whereStudents) {
+				System.out.println(tempStudent);
+			}
+			//commit transaction
+			session.getTransaction().commit();
+			 
+			//get new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			//List with where, or
+			List<Student> whereOrStudents = session
+					.createQuery("from Student s where s.lastName LIKE '%o%'"
+							+ "OR s.firstName LIKE '%o%'")
+					.getResultList();
+			System.out.println("whereOrStudents: ");
+			for (Student tempStudent : whereOrStudents) {
+				System.out.println(tempStudent);
+			}
+			//commit transaction
+			session.getTransaction().commit();
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
